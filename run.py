1f6dd94cde900eb6309725bea59a57d6c69ec1aa
DOdrums <dirkornee@hotmail.com>
2022-05-30 15:07:57 +0000

    write function to get food and item data and display it

--- run.py
+++ run.py
@@ -12,9 +12,29 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("smart_house_inventory")
 
-food = SHEET.worksheet("food")
-item = SHEET.worksheet("item")
 
-data = food.get_all_values()
+def get_stock_data():
+    """
+    function to display all the stock of the house
+    """
+    new_line = '\n'
+    food = SHEET.worksheet("food")
+    item = SHEET.worksheet("item")
 
-print(data)
+    data_food_raw = food.get_all_values()
+    data_food = ""
+    data_item = item.get_all_values()
+
+    for product in data_food_raw:
+        for data in product:
+            data_food += f"{data} |"
+        data_food += f"{new_line}"
+
+    return (
+        f"These are all the items you have in your house:{new_line}"
+        f"{data_item}{new_line}"
+        f"This is all the food you have in your house:{new_line}{data_food}"
+    )
+
+
+print(get_stock_data())
