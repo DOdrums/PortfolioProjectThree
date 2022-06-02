11de8f5aa9a488fc5188b2600a5a1e8cfc76211c
DOdrums <dirkornee@hotmail.com>
2022-06-02 10:01:25 +0000

    add method to product class to update worksheet

--- run.py
+++ run.py
@@ -14,6 +14,8 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("smart_house_inventory")
+food = SHEET.worksheet("food")
+item = SHEET.worksheet("item")
 
 
 def get_stock_data():
@@ -21,8 +23,6 @@ def get_stock_data():
     function to display all the stock of the house
     """
     new_line = '\n'
-    food = SHEET.worksheet("food")
-    item = SHEET.worksheet("item")
 
     data_food_raw = food.get_all_values()
     data_food = ""
@@ -58,6 +58,19 @@ class Product:
         self.days_per = days_per
         self.date_added = datetime.datetime.now()
 
+    def add_item(self):
+        """
+        add item data to google sheet
+        """
+        data = [self.name, self.amount, self.days_per, str(self.date_added)]
+        item.append_row(data)
 
-afwasborstel = Product("", 0, 0)
-print(afwasborstel.__dict__)
+
+afwasborstel = Product("hank", 0, 0)
+print(afwasborstel.name)
+# print(get_stock_data())
+afwasborstel.add_item()
+
+
+# for delete function, show names with row number and let user enter number to delete data
+# make seperate function for calculating days left/items left (called by get stock data function)
